d7d53e59613eba7cbd7145c6c481a0a84572db9f
Martin Smith <martin@mbs3.org>
2015-01-08 10:52:47 -0600

    Package custom matchers, bump version

---
+++ libraries/matchers.rb
@@ -0,0 +1,20 @@
+# cookbook/libraries/matchers.rb
+
+if defined?(ChefSpec)
+  def replace_xml(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:xmledit, :replace, resource_name)
+  end
+
+  def append_xmledit(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:xmledit, :append, resource_name)
+  end
+
+
+  def remove_xmledit(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:xmledit, :remove, resource_name)
+  end
+
+  def append_if_missing_xmledit(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:xmledit, :append_if_missing, resource_name)
+  end
+end
