78b06dc8c05095673a5e11bc76d322e72d338b39
Martin Smith <martin@mbs3.org>
2015-01-08 11:23:51 -0600

    Matchers had wrong resource name

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -2,19 +2,19 @@
 
 if defined?(ChefSpec)
   def replace_xml(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:xmledit, :replace, resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:xml_edit, :replace, resource_name)
   end
 
   def append_xml(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:xmledit, :append, resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:xml_edit, :append, resource_name)
   end
 
 
   def remove_xml(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:xmledit, :remove, resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:xml_edit, :remove, resource_name)
   end
 
   def append_if_missing_xml(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:xmledit, :append_if_missing, resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:xml_edit, :append_if_missing, resource_name)
   end
 end
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'martin@mbs3.org'
 license          'apache2'
 description      'Provides a resource and provider for incremental editing of XML files'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.3'
+version          '1.0.4'
 
 depends 'poise'
 depends 'xml'
