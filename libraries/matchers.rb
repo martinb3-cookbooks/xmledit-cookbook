316f906a1ec619989ef89b830963d5493d31181f
Christoph <cgriesshammer@gmail.com>
2016-04-21 11:11:07 -0400

    Issue #15: ChefSpec matcher for xml_edit bulk edit

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -16,4 +16,8 @@ if defined?(ChefSpec)
   def append_if_missing_xml(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:xml_edit, :append_if_missing, resource_name)
   end
+
+  def bulk_edit_xml(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:xml_edit, :bulk, resource_name)
+  end
 end
